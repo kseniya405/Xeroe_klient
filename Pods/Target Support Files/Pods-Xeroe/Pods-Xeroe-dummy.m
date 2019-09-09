54be2956e832805f7a1c1661716a4af2a2744486
Denis Markov <keynsianets@gmail.com>
2019-09-09 14:26:55 +0300

    all files

---
+++ Pods/Target Support Files/Pods-Xeroe/Pods-Xeroe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Xeroe : NSObject
+@end
+@implementation PodsDummy_Pods_Xeroe
+@end
---
+++ Xeroe/LoginVC/File.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  Xeroe
+//
+//  Created by Денис Марков on 9/2/19.
+//  Copyright © 2019 Денис Марков. All rights reserved.
+//
+
+import Foundation
+
