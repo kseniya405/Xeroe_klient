51fb0dc589479156809d13d7c6b32d6772029fb3
Denis Markov <keynsianets@gmail.com>
2019-09-20 12:20:38 +0300

    correct driverWayVC

--- Xeroe/ReportViewController.swift
+++ Xeroe/ReportViewController.swift
@@ -19,8 +19,8 @@ class ReportViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+//        tableView.delegate = self
+//        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
@@ -29,15 +29,15 @@ class ReportViewController: UIViewController {
 
 }
 
-extension ReportViewController: UITableViewDelegate, UITableViewDataSource {
+//extension ReportViewController: UITableViewDelegate, UITableViewDataSource {
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        //
+//    }
+
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-    
-}
+
